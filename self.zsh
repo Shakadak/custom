5b23aa0763a3baf19843e350b6a5e1ec28e7e28d
shakadak <nathanael.pineau@gmail.com>
2015-06-04 18:00:29 +0200

    haskell commandes

--- self.zsh
+++ self.zsh
@@ -1,3 +1,5 @@
 export MAIL=npineau@student.42.fr
 alias mount_rust="hdiutil attach -mountpoint ~/Rust ~/img/Rust.sparseimage"
 alias unmount_rust="hdiutil eject ~/Rust"
+alias mount_haskell="hdiutil attach -mountpoint ~/Haskell ~/img/Haskell.sparseimage"
+alias unmount_haskell="hdiutil eject ~/Haskell"
