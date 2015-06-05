a080e590a01abcda389b677ca3fa66c1d5934919
shakadak <nathanael.pineau@gmail.com>
2015-06-05 10:17:47 +0200

    brew mount point

--- self.zsh
+++ self.zsh
@@ -1,5 +1,7 @@
 export MAIL=npineau@student.42.fr
+
 alias mount_rust="hdiutil attach -mountpoint ~/Rust ~/img/Rust.sparseimage"
 alias unmount_rust="hdiutil eject ~/Rust"
-alias mount_haskell="hdiutil attach -mountpoint ~/Haskell ~/img/Haskell.sparseimage"
-alias unmount_haskell="hdiutil eject ~/Haskell"
+
+alias mount_brew="hdiutil attach -mountpoint ~/.brew ~/img/brew.sparseimage"
+alias unmount_brew="hdiutil eject ~/.brew"
