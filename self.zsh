772526110ca9f8bdd6c0b92803b8a2ab5020ba04
shakadak <nathanael.pineau@gmail.com>
2015-06-05 11:55:07 +0200

    cabal

--- self.zsh
+++ self.zsh
@@ -3,6 +3,9 @@ export MAIL=npineau@student.42.fr
 alias mount_rust="hdiutil attach -mountpoint ~/Rust ~/img/Rust.sparseimage"
 alias unmount_rust="hdiutil eject ~/Rust"
 
+alias mount_cabal="hdiutil attach -mountpoint ~/.cabal ~/img/cabal.sparseimage"
+alias unmount_cabal="hdiutil eject ~/.cabal"
+
 alias mount_brew="hdiutil attach -mountpoint ~/.brew ~/img/brew.sparseimage"
 alias unmount_brew="hdiutil eject ~/.brew"
 
