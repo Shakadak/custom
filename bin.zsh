d70fd711575b3b731318eb75abba88cc03ed3bf7
nathanael <nathanael.pineau@gmail.com>
2015-07-10 18:08:18 +0200

    moving haskell export to its own file

--- bin.zsh
+++ bin.zsh
@@ -1,5 +1,2 @@
-export GHC_HOME=$HOME/Development/bin/ghc
-
 # Add brew binary as first option and Rust as second, the rest comes after.
 export PATH=$HOME/.brew/bin:$HOME/Rust/bin:$PATH
-export PATH=$GHC_HOME/bin:$PATH
---
+++ haskell.zsh
@@ -0,0 +1,5 @@
+export GHC_HOME=$HOME/Development/bin/ghc
+export PATH=$GHC_HOME/bin:$PATH
+
+export CABAL_HOME=$HOME/.cabal
+export PATH=$CABAL_HOME/bin:$PATH
