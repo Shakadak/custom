3d942ac9fd12619600a5e7a698df28dbb9681f2f
Nathanael PINEAU <npineau@student.42.fr>
2015-11-19 18:35:33 +0100

    pathy

--- haskell.zsh
+++ haskell.zsh
@@ -1,5 +1,5 @@
 # Add GHC 7.10.2 to the PATH, via https://ghcformacosx.github.io/
-export GHC_DOT_APP="/nfs/zfs-student-5/users/2013/npineau/dev/bin/ghc-7.10.2.app"
+export GHC_DOT_APP="$HOME/dev/bin/ghc-7.10.2.app"
 if [ -d "$GHC_DOT_APP" ]; then
   export PATH="${HOME}/.local/bin:${HOME}/.cabal/bin:${GHC_DOT_APP}/Contents/bin:${PATH}"
 fi
