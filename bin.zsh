0aea1be7fa6634645351b59959f84dd84c488e1c
Nathanael PINEAU <npineau@student.42.fr>
2017-12-01 07:51:39 +0100

    tudududu

--- bin.zsh
+++ bin.zsh
@@ -1,6 +1,6 @@
 # Add brew binary as first option and USB key as second, the rest comes after.
 
-alias clang="clang-3.7"
-alias clang++="clang++-3.7"
+#alias clang="clang-3.7"
+#alias clang++="clang++-3.7"
 
 alias lua="lua5.3"
---
+++ go.zsh
@@ -0,0 +1,5 @@
+export PATH=$PATH:/Users/npineau/.brew/opt/go/libexec/bin
+
+export GOPATH=$HOME/golang
+
+export PATH=$PATH:$GOPATH/bin
